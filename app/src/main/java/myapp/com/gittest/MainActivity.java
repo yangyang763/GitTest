4566721b3861e377bac50482137af08d79a7ad52
samsung <yangyang763@126.com>
2016-04-29 15:36:08 +0800

    git git  git add first log add log

--- app/src/main/java/myapp/com/gittest/MainActivity.java
+++ app/src/main/java/myapp/com/gittest/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("","=======  第一次添加log");
+        Log.i("","======  第二次添加log！！");
     }
 }
